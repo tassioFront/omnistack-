4a2269749cffaa7d87d9a480925b59e7ccacec60
tassio72 <tassio.silva@e-midia.com>
2020-03-12 13:08:36 -0300

    front

---
+++ front/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import  { BrowserRouter, Switch, Route} from 'react-router-dom'
+import Login from './pages/Login/Index'
+import Dashboard from './pages/Dashboard/Index'
+import New from './pages/New/Index'
+
+export default function Routes() {
+    return (
+        <BrowserRouter> 
+            <Switch>
+                <Route path="/" exact component={Login} ></Route>
+                <Route path="/dashboard"  component={Dashboard} ></Route>
+                <Route path="/new"  component={New} ></Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
